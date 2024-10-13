6ac96efea8aa7576d4ea17d086c95e09a0736c6a
Ali Irani <ali.irani@ovou.com>
2024-10-13 13:16:25 +0330

    Simple callback query

--- src/keyboards.ts
+++ src/keyboards.ts
@@ -2,10 +2,8 @@ import { InlineKeyboard } from "grammy";
 import { SessionState } from "./session";
 import { CONFIRM_BUTTON_TEXT, CANCEL_BUTTON_TEXT } from "./consts";
 
-export const CONFIRM_CALLBACK_QUERY =
-    `${SessionState.AWAITING_PHONE}_CONFIRM`.toLowerCase();
-export const CANCEL_CALLBACK_QUERY =
-    `${SessionState.AWAITING_PHONE}_CANCEL`.toLowerCase();
+export const CONFIRM_CALLBACK_QUERY = "confirm";
+export const CANCEL_CALLBACK_QUERY = "cancel";
 const labelDataPairs = [
     [CONFIRM_BUTTON_TEXT, CONFIRM_CALLBACK_QUERY],
     [CANCEL_BUTTON_TEXT, CANCEL_CALLBACK_QUERY]
